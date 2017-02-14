b7bafab6366176ed1f34958e3de295517f345bc5
user <user@user-PC>
2017-02-14 20:47:49 +0200

    Autonomous added in

--- src/org/usfirst/frc/team5554/robot/Commands/DriveDistance.java
+++ src/org/usfirst/frc/team5554/robot/Commands/DriveDistance.java
@@ -5,28 +5,20 @@ import org.usfirst.frc.team5554.robot.Driver;
 
 public class DriveDistance extends Command
 {
-	int valuefromencoder;
 	int wantedValue;
-	double speed;
+	Driver driver;
 	
-	public DriveDistance(int _wantedValue)
+	public DriveDistance(int _wantedValue, Driver _driver)
 	{
 		super("Drive Distance");
 		wantedValue = _wantedValue;
-		valuefromencoder = 0;
-		speed = 0;
+		driver = _driver;
 	}
 	
 	@Override
 	public void initialize()
 	{		
-
-		while(valuefromencoder != wantedValue)
-		{
-			Driver.Drive(speed);
-		}
-		
-		
+		//driver.
 		System.out.println("We drove the distance");
 	}
 	
